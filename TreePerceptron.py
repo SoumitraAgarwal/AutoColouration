d2cb5774158bb9d1a998b6e065dfb1038ef240df
SoumitraAgarwal <agarwalsoumitra1504@gmail.com>
2017-10-25 21:29:30 +0530

    Completed patch creation

--- TreePerceptron.py
+++ TreePerceptron.py
@@ -1,13 +1,7 @@
+import random
 import math
+import os
 import cv2
-
-
-patchSize		= 10
-trainDirectory 	= "Fishes_TRAIN"
-testDirectory	= "Fishes_TEST"
-patchDirectory	= "Patches"
-
-
 
 class Tree(object):
     def __init__(self):
@@ -15,9 +9,27 @@ class Tree(object):
         self.right = None
         self.data = None
 
-def createPatches(patchSize, trainDirectory):
+def createDir(Directory):
+
+	if(Directory not in os.listdir('.')):
+		os.mkdir(Directory)
 
 
+def createPatches(patchSize, trainDirectory, patchDirectory, pathchesImage):
+	images = os.listdir(trainDirectory)
+	for image in images:
+
+		print("Creating patch for image " + image)
+		createDir(patchDirectory)
+		img 	= cv2.imread(trainDirectory + "/" + image) 
+
+		for patch in range(pathchesImage):
+
+			x 	= random.randint(patchSize, img.shape[0] - patchSize)
+			y 	= random.randint(patchSize, img.shape[1] - patchSize)
+
+			tpatch = img[y : y + patchSize, x : x + patchSize]
+			cv2.imwrite(patchDirectory + '/' + image + "_" + str(patch) + ".JPEG", tpatch)
 
 
 def initialiseTree():
@@ -28,5 +40,16 @@ def initialiseTree():
 
 
 if __name__ == "__main__":
+
+
+	patchSize		= 8
+	pathchesImage	= 100
+	trainDirectory 	= "Fishes_TRAIN"
+	testDirectory	= "Fishes_TEST"
+	patchDirectory	= "Patches"
+
 	root = initialiseTree()
-	createPatches(patchSize, trainDirectory)
+	# createPatches(patchSize, trainDirectory, patchDirectory, pathchesImage)
+
+
+	
