6e184008b4b1ef2e5040994b3e88864604239c2d
SoumitraAgarwal <agarwalsoumitra1504@gmail.com>
2017-10-26 14:43:23 +0530

    Testing addition

--- TreePerceptron.py
+++ TreePerceptron.py
@@ -1,5 +1,6 @@
 from operator import add
 import numpy as np, numpy.random
+import pickle
 import random
 import math
 import os
@@ -69,10 +70,10 @@ def trainTree(root, trainDirectory, channel, trainingRate):
 		if(flag == 0):
 			
 			# print(lprediction - results[len(results)/2])
-			normaliser 			= -trainingRate*(lprediction - results[len(results)/2])
+			normaliser 			= -1.0*trainingRate*(lprediction - results[len(results)/2])/counter
 			normalisedAdd 		= [i*normaliser for i in features]
 			root.left.weights 	= addLists(root.left.weights, normalisedAdd)
-			normaliser 			= -trainingRate*(rprediction - results[len(results)/2])
+			normaliser 			= -1.0*trainingRate*(rprediction - results[len(results)/2])/counter
 			normalisedAdd 		= [i*normaliser for i in features]
 			root.right.weights 	= addLists(root.right.weights, normalisedAdd)
 			flag 				= 1
@@ -84,10 +85,57 @@ def trainTree(root, trainDirectory, channel, trainingRate):
 			flag 				= 0
 
 
-def testResults(img)
+def testResults(testDirectory, storeModels, r, g, b):
+	createDir(storeResults)
+	images 	= os.listdir(testDirectory)
+	counter = 1
+
+	redTree 	= pickle.load( open( r, "rb" ) )
+	greenTree 	= pickle.load( open( g, "rb" ) )
+	blueTree 	= pickle.load( open( b, "rb" ) )
+
+	for image in images:
+		print("Tested for " + str(counter) + "images out of " + str(len(images)))
+		counter	   	+= 1 
+		bgimg 		= cv2.imread(trainDirectory + "/" + image, 0)
+
+		result_img	= []
+		for i in range(bgimg.shape[0]):
+
+			result_row = []
+			for j in range(bgimg.shape[1]):
+				
+				patch = getPatch(bgimg, i, j)
+				features 	= getFeatureImage(patch)
+				
+				redlprediction		= np.dot(redTree.left.weights, features)
+				redrprediction  	= np.dot(redTree.right.weights, features)
+				greenlprediction	= np.dot(greenTree.left.weights, features)
+				greenrprediction  	= np.dot(greenTree.right.weights, features)
+				bluelprediction		= np.dot(blueTree.left.weights, features)
+				bluerprediction		= np.dot(blueTree.left.weights, features)
+
+def getPatch(img, i, j):
+	if(i<patch_size/2):
+		i = patchSize/2;
+	if(j<patchSize/2):
+		j = patchSize/2;
+
+	if(i>img.shape[0]-patchSize/2 - 1):
+		i = img.shape[0]-patchSize/2 - 1
+
+	if(j>img.shape[1]-patchSize/2 - 1):
+		j = img.shape[0]-patchSize/2 - 1
+
+	return img[j - patchSize/2:j + patchSize/2, i - patchSize/2 : i + patchSize/2]
 
 def getFeatureImage(img):
 	return img.flatten()
+
+def pickleRes(var, name, Directory):
+	createDir(Directory)
+	pickle.dump(var, open(Directory + '/' + name + ".p", "wb" ) )
+
 
 def addLists(list1, list2):
 	return [sum(x) for x in zip(list1, list2)]
@@ -100,10 +148,12 @@ if __name__ == "__main__":
 	trainDirectory 	= "Fishes_TRAIN"
 	testDirectory	= "Fishes_TEST"
 	patchDirectory	= "Patches"
+	storeResults 	= "Results"
+	storeModels 	= "Models"
 	lweights 		= np.random.dirichlet(np.ones(patchSize*patchSize),size=1)
 	rweights 		= np.random.dirichlet(np.ones(patchSize*patchSize),size=1)
-	probab 			= [0.5, 0.5]
-	trainingRate 	= 0.000001
+	probab 			= [0.5,0.5]
+	trainingRate 	= 0.001
 
 	redTree			= initialiseTree(rweights, lweights, probab)
 	
@@ -119,10 +169,12 @@ if __name__ == "__main__":
 
 	# createPatches(patchSize, trainDirectory, patchDirectory, pathchesImage)
 
-	trainTree(redTree, patchDirectory, 0, trainingRate)
-	trainTree(greenTree, patchDirectory, 1, trainingRate)
-	trainTree(blueTree, patchDirectory, 2, trainingRate)
-
-	testResults(testDirectory, storeResults, redTree, greenTree, blueTree)
+	# trainTree(redTree, patchDirectory, 0, trainingRate)
+	# pickleRes(redTree, 'red', storeModels)
+	# trainTree(greenTree, patchDirectory, 1, trainingRate)
+	# pickleRes(greenTree, 'green', storeModels)
+	# trainTree(blueTree, patchDirectory, 2, trainingRate)
+	# pickleRes(blueTree, 'blue', storeModels)
+	testResults(testDirectory, patchSize, storeModels, 'red.p', 'green.p', 'blue.p')
 
 	
