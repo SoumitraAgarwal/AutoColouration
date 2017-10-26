660dbacdaf9aac937258b6b658acc4916d5d98c5
SoumitraAgarwal <agarwalsoumitra1504@gmail.com>
2017-10-26 12:53:45 +0530

    Completed training

--- TreePerceptron.py
+++ TreePerceptron.py
@@ -1,3 +1,5 @@
+from operator import add
+import numpy as np, numpy.random
 import random
 import math
 import os
@@ -5,9 +7,9 @@ import cv2
 
 class Tree(object):
     def __init__(self):
-        self.left = None
-        self.right = None
-        self.data = None
+        self.left 		= None
+        self.right 		= None
+        self.weights 	= None
 
 def createDir(Directory):
 
@@ -32,24 +34,85 @@ def createPatches(patchSize, trainDirectory, patchDirectory, pathchesImage):
 			cv2.imwrite(patchDirectory + '/' + image + "_" + str(patch) + ".JPEG", tpatch)
 
 
-def initialiseTree():
+def initialiseTree(rweights, lweights, probab):
+
 	root = Tree() 
+	root.weights = probab
 	root.left = Tree()
+	root.left.weights = lweights
 	root.right = Tree()
+	root.right.weights = rweights
 	return root
 
+def trainTree(root, trainDirectory, channel, trainingRate):
+	
+	flag 	= 0
+	counter = 1
+	images 	= os.listdir(trainDirectory)
+	for image in images:
+
+
+		if(counter%1000 == 0):
+			print("Trained for " + str(counter) + " patches out of " + str(len(images)))
+
+		img 		= cv2.imread(trainDirectory + "/" + image)
+		bgimg 		= cv2.imread(trainDirectory + "/" + image, 0)
+		channels	= cv2.split(img)
+		features 	= getFeatureImage(bgimg)
+		results		= getFeatureImage(channels[channel])
+
+		lprediction	= np.dot(root.left.weights, features)
+		rprediction = np.dot(root.right.weights, features)
+		counter	   	+= 1 		
+
+		# print(root.weights)
+		if(flag == 0):
+			
+			# print(lprediction - results[len(results)/2])
+			normaliser 			= -trainingRate*(lprediction - results[len(results)/2])
+			normalisedAdd 		= [i*normaliser for i in features]
+			root.left.weights 	= addLists(root.left.weights, normalisedAdd)
+			normaliser 			= -trainingRate*(rprediction - results[len(results)/2])
+			normalisedAdd 		= [i*normaliser for i in features]
+			root.right.weights 	= addLists(root.right.weights, normalisedAdd)
+			flag 				= 1
+
+		
+		else:
+			root.weights[0] 	= (1 - trainingRate*(lprediction - results[len(results)/2]))*root.weights[0]
+			root.weights[1] 	= 1 - root.weights[0] 
+			flag 				= 0
+
+
+def getFeatureImage(img):
+	return img.flatten()
+
+def addLists(list1, list2):
+	return [sum(x) for x in zip(list1, list2)]
 
 if __name__ == "__main__":
 
 
-	patchSize		= 8
+	patchSize		= 9
 	pathchesImage	= 100
 	trainDirectory 	= "Fishes_TRAIN"
 	testDirectory	= "Fishes_TEST"
 	patchDirectory	= "Patches"
+	# lweights 		= np.random.dirichlet(np.ones(patchSize*patchSize),size=1)
+	# rweights 		= np.random.dirichlet(np.ones(patchSize*patchSize),size=1)
+	lweights 		= np.array([0]*(patchSize*patchSize))
+	rweights 		= np.array([0]*(patchSize*patchSize))
+	probab 			= [0.5, 0.5]
+	trainingRate 	= 0.000001
 
-	root = initialiseTree()
+	redTree			= initialiseTree(rweights, lweights, probab)
+	greenTree 		= initialiseTree(rweights, lweights, probab)
+	blueTree 		= initialiseTree(rweights, lweights, probab)
+
 	# createPatches(patchSize, trainDirectory, patchDirectory, pathchesImage)
 
+	trainTree(redTree, patchDirectory, 0, trainingRate)
+	# trainTree(greenTree, patchDirectory, 1, trainingRate)
+	# trainTree(blueTree, patchDirectory, 2, trainingRate)
 
 	
