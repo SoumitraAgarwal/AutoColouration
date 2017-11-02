0b5de8e464291dbd5504888d96eb81ca2a9ba853
SoumitraAgarwal <agarwalsoumitra1504@gmail.com>
2017-11-02 11:56:02 +0530

    Add test code

--- __main__.py
+++ __main__.py
@@ -9,30 +9,32 @@ if __name__ == "__main__":
 
 	patchSize		= 9
 	pathchesImage	= 100
-	iterations 		= 100
+	iterations 		= 30
 	trainDirectory 	= "Fishes_TRAIN"
 	testDirectory	= "Fishes_TEST"
 	patchDirectory	= "Patches"
-	storeResults 	= ["Results_Blue", "Results_Green", "Results_Red"]
+	storeResults 	= "Results"
 	storeModels 	= "Models"
-	lweights 		= np.random.dirichlet(np.ones(patchSize*patchSize),size=1)
-	rweights 		= np.random.dirichlet(np.ones(patchSize*patchSize),size=1)
-	lb 				= 1
-	rb 				= 1
-	trainingRate 	= 0.01
+	lweights 		= [np.random.uniform(0,1,patchSize*patchSize)]
+	rweights 		= [np.random.uniform(0,1,patchSize*patchSize)]
+	# lweights 		= np.random.dirichlet(np.ones(patchSize*patchSize),size=1)
+	# rweights 		= np.random.dirichlet(np.ones(patchSize*patchSize),size=1)
+	lb 				= lweights[0][0]*rweights[0][0]
+	rb 				= rweights[0][0]*lweights[0][0]
+	trainingRate 	= 0.1
 
-	redTree			= treemodels.initialiseTree(lweights[0], rweights[0], lb, rb)
-	greenTree 		= treemodels.initialiseTree(lweights[0], rweights[0], lb, rb)
-	blueTree 		= treemodels.initialiseTree(lweights[0], rweights[0], lb, rb)
+	# redTree			= treemodels.initialiseTree(lweights[0], rweights[0], lb, rb)
+	# greenTree 		= treemodels.initialiseTree(lweights[0], rweights[0], lb, rb)
+	# blueTree 		= treemodels.initialiseTree(lweights[0], rweights[0], lb, rb)
 
 	# utility.createPatches(patchSize, trainDirectory, patchDirectory, pathchesImage)
 
-	neural.trainTree(redTree, patchDirectory, 0, trainingRate, iterations)
-	utility.pickleRes(redTree, 'red', storeModels)
-	trainTree(greenTree, patchDirectory, 1, trainingRate)
-	pickleRes(greenTree, 'green', storeModels)
-	trainTree(blueTree, patchDirectory, 2, trainingRate)
-	pickleRes(blueTree, 'blue', storeModels)
-	# neural.testResults(testDirectory, patchSize, storeModels, storeResults, 'red.p', 'green.p', 'blue.p')
+	# neural.trainTree(redTree, patchDirectory, 0, trainingRate, iterations)
+	# utility.pickleRes(redTree, 'red', storeModels)
+	# neural.trainTree(greenTree, patchDirectory, 1, trainingRate, iterations)
+	# utility.pickleRes(greenTree, 'green', storeModels)
+	# neural.trainTree(blueTree, patchDirectory, 2, trainingRate, iterations)
+	# utility.pickleRes(blueTree, 'blue', storeModels)
+	neural.testResults(testDirectory, patchSize, storeModels, storeResults, 'red.p', 'green.p', 'blue.p')
 
 	
